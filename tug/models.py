abd5191a472a582c9c0704c7c7628c9741afde2a
armen <armen@igitian.net>
2013-05-13 21:55:04 +0300

    old tug app, let it live for a little longer

---
+++ tug/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+class TrainingType(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=150)
+
+class TrainingSession(models.Model):
+    date = models.DateTimeField("session's datetime")
+    user = models.ForeignKey(User)
+    training_type = models.ForeignKey(TrainingType)
+    feedback = models.CharField(max_length=250)
+
+class TrainingSet(models.Model):
+    training_session = models.ForeignKey(TrainingSession)
+    set_number = models.IntegerField()
+    reps = models.IntegerField()
---
+++ tug/views.py
@@ -0,0 +1,16 @@
+# Create your views here.
+
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the pyramid index.")
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
