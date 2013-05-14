05f68e1c910f53fab64c0f922f249f6be9caf20a
armen <armen@igitian.net>
2013-05-14 03:01:15 +0300

    race: added index view

--- race/views.py
+++ race/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+# creating views for the race app
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from race.models import User, Session
+
+# index view, show last 5 sessions
+def index(request):
+    latest_session_list = Session.objects.order_by('-date')[:5]
+    context = { 'latest_session_list': latest_session_list }
+    return render(request, 'race/index.html', context)
