9f7c480fc52de42302d26477dc57394ec6cae72b
armen <armen@igitian.net>
2013-05-14 10:25:55 +0300

    race: remapped 'user' urls

--- race/urls.py
+++ race/urls.py
@@ -6,5 +6,6 @@ from race import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<user_id>\d+)/users/$', views.user, name='user'),
 )
 
--- race/views.py
+++ race/views.py
@@ -6,6 +6,15 @@ from race.models import User, Session
 
 # index view, show last 5 sessions
 def index(request):
+    users = Users.objects.all()
     latest_session_list = Session.objects.order_by('-date')[:5]
+    
     context = { 'latest_session_list': latest_session_list }
     return render(request, 'race/index.html', context)
+
+def user(request, user_id):
+    try:
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        raise Http404
+    return render(request, 'race/user.html', {'user': user})
