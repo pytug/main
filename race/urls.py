e7dd02a10ab832fd1bd6df22339486b7bab5894a
armen <armen@igitian.net>
2013-05-22 14:02:07 +0300

    race: changed url mapping for user view to user/<user_id>/ from <user_id>/users/

--- race/urls.py
+++ race/urls.py
@@ -6,6 +6,7 @@ from race import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<user_id>\d+)/users/$', views.user, name='user'),
+    url(r'^user/(?P<user_id>\d+)/$', views.user, name='user'),
+#    url(r'^(?P<user_id>\d+)/users/$', views.user, name='user'),
 )
 
