27c967ebaff4f5a4b7ca036791755ce6c8d9147f
armen <armen@igitian.net>
2013-05-22 11:28:53 +0300

    race: added user's max repetitions lookup

--- race/views.py
+++ race/views.py
@@ -4,14 +4,11 @@ from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render
 from race.models import User, Session
+from django.db.models import Max
 
 # index view, show last 5 sessions
 def index(request):
     latest_session_list = Session.objects.order_by('-date')[:5]
-    
-#not quite yet needed
-#    users = User.objects.all()
-    
     context = { 'latest_session_list': latest_session_list }
     return render(request, 'race/index.html', context)
 
@@ -20,11 +17,13 @@ def user(request, user_id):
         user = User.objects.get(pk=user_id)
         user_session_count = user.session_set.count()
         user_session_list = Session.objects.filter(user_id=user_id).order_by('-date')
+        user_session_max = user_session_list.aggregate(Max('reps'))
         template = loader.get_template('race/user.html')
         context = Context({
             'user_session_list': user_session_list,
             'user': user,
             'user_session_count': user_session_count,
+            'user_session_max': user_session_max,
         })
     except User.DoesNotExist:
         raise Http404
